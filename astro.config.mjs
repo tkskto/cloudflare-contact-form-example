065691a287f23846836382b53d99bca10debe516
tkskto <tkskto@gmail.com>
2025-07-03 17:52:04 +0900

    Astroの設定を調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: 'preserve',
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true
